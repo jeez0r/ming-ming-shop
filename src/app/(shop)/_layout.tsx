90fafa21a7ac159d05ba9d685d61461bd24b1fac
jeez0r <jhayarmagnaye1@gmail.com>
2024-10-21 22:22:48 +0800

    1st commit

---
+++ src/app/(shop)/_layout.tsx
@@ -0,0 +1,11 @@
+import {Tabs} from "expo-router"
+const Tabslayout  = () => {
+return(
+ <Tabs>
+    <Tabs.Screen name ="index" options={{}}/>
+    <Tabs.Screen name ="orders" options={{}}/>
+</Tabs>
+    );
+};
+
+export default Tabslayout;
---
+++ src/app/(shop)/index.tsx
@@ -0,0 +1,13 @@
+import { Button, StyleSheet, Text, View } from 'react-native'
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Hoame</Text>
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
